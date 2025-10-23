a0930b27c8a5256ff2f90e95154af357d6e256a3
doniezak <doniezak@terpmail.umd.edu>
2025-10-23 11:04:08 -0400

    Create lab7q1.py

---
+++ lab7q1.py
@@ -0,0 +1,14 @@
+import RPI GPIO as GPIO
+import socket
+
+HOST = ''
+PORT = 8080
+
+<html>
+<head><title> Brightness Level: </title></head>
+<body>
+  <button name="two_buttons" value="b1"> Click here </button>
+  <button name="two_buttons" value="b2"> Or here </button>
+</form>
+</html>
+
